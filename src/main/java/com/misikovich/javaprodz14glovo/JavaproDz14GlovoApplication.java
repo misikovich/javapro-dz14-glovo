135933f0155a5c1f91af60d6f2aa18b5a59e56d5
misikovich <misikovich@gmail.com>
2023-04-18 18:02:53 +0200

    first

---
+++ src/main/java/com/misikovich/javaprodz14glovo/JavaproDz14GlovoApplication.java
@@ -0,0 +1,13 @@
+package com.misikovich.javaprodz14glovo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaproDz14GlovoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaproDz14GlovoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/misikovich/javaprodz14glovo/JavaproDz14GlovoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misikovich.javaprodz14glovo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaproDz14GlovoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
